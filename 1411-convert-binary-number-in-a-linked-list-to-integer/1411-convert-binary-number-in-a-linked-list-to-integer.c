7981fdf98a0489f80796a941d93204cf1cfab3b8
Subham banerjee <114326247+Subby575@users.noreply.github.com>
2024-07-18 11:32:34 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.04%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.c
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.c
@@ -6,20 +6,19 @@
  * };
  */
 int getDecimalValue(struct ListNode* head) {
-    int* arr=(int*)malloc(sizeof(int)*30);
-    struct ListNode* temp=head;
-    int i=0,val=0;
-    while(temp)
-    {
-        arr[i]=temp->val;
-        temp=temp->next;
+    int* arr = (int*)malloc(sizeof(int) * 30);
+    struct ListNode* temp = head;
+    int i = 0, val = 0;
+    while (temp) {
+        arr[i] = temp->val;
+        temp = temp->next;
         i++;
     }
-    int k=i-1;
-    for(int j=0;j<i;j++){
-        val+=(pow(2,k)*arr[j]);
+    int k = i - 1;
+    for (int j = 0; j < i; j++) {
+        val += (pow(2, k) * arr[j]);
         k--;
-        printf("%d ",val);
+        printf("%d ", val);
     }
     return val;
 }
