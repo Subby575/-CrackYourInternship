f2a9df70eb62f66d3c1ddae2dcde5196b853021a
Subham banerjee <114326247+Subby575@users.noreply.github.com>
2024-07-20 12:23:38 +0530

    Time: 0 ms (100%), Space: 10.1 MB (7.03%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.c
+++ 0088-merge-sorted-array/0088-merge-sorted-array.c
@@ -1,38 +1,17 @@
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
-    int i=0,j=0,k=0,l=0;
-        int* arr=(int*)malloc(sizeof(int)*(nums1Size));
-        while(i<m && j<n & k<(m+n))
+    int i=m-1,j=n-1,k=m+n-1;
+    while(j>=0)
+    {
+        if(i>=0 && nums1[i]>nums2[j])
         {
-            if(nums1[i]<=nums2[j])
-            {
-                arr[k]=nums1[i];
-                k++;
-                i++;
-
-            }
-            else if(nums1[i]>nums2[j]){
-                arr[k]=nums2[j];
-                k++;
-                j++;
-            }
-            // printf(" %d i=%d , j=%d",arr[k-1],i,j);
+            nums1[k]=nums1[i];
+            i--;
         }
-        while(i<m)
+        else
         {
-            arr[k]=nums1[i];
-            i++;
-            k++;
+            nums1[k]=nums2[j];
+            j--;
         }
-        while(j<n)
-        {
-            arr[k]=nums2[j];
-            j++;
-            k++;
-        // printf(" %d ",arr[k-1]);
-        }
-        while(l<nums1Size){
-            nums1[l]=arr[l];
-            l++;
-        }
-        free(arr);
+        k--;
     }
+}
